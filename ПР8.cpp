e7c2ffd109d04ef02378a09ba127be2b58f9ef04
BohdanProkaza <45563904+BohdanProkaza@users.noreply.github.com>
2018-12-17 09:40:44 +0200

    Update ПР8.cpp

--- ПР8.cpp
+++ ПР8.cpp
@@ -1,88 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
-	
-int main (){
-	float a[102][102], b[102][102];
-	float  a1[102] = {0}, b1[102] = {0};
-	int n;
-	
-	cin >> n;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			cin >> a[i][j];	
-			if(i==j){
-				a1[i] = a[i][j];
-			}
-		}		
-	}
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			cin >> b[i][j];	
-			if(i==j){
-				b1[i] = b[i][j];
-			}			
-		}		
-	}
-	
-	cout << endl << " a before" << endl;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			cout << a[i][j] << " ";							
-		}
-		cout << endl;
-	}
-	cout << endl << " b before" << endl;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			cout << b[i][j] << " ";							
-		}
-		cout << endl;
-	}
-	
-	
-//	cout << endl << " before"<< endl << endl;
-//	for(int i=1;i<=n;i++){
-//		cout << a1[i] << " "; 
-//	}	cout << endl;
-//	cout << endl << " before"<< endl << endl;
-//	for(int i=1;i<=n;i++){
-//		cout << b1[i] << " "; 
-//	}	cout << endl;
-	
-	sort(a1+1,a1+n+1);
-	sort(b1+1,b1+n+1);
-	
-//	cout << endl << " after"<< endl << endl;
-//	for(int i=1;i<=n;i++){
-//		cout << a1[i] << " "; 
-//	}	cout << endl;
-//	cout << endl << " after"<< endl << endl;
-//	for(int i=1;i<=n;i++){
-//		cout << b1[i] << " "; 
-//	}	cout << endl;
-	
-	for(int i=1;i<=n;i++){
-		a[i][i] = b1[i];
-		b[i][i] = a1[i];
-	}
-	
-	cout << endl << " a after" << endl;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			cout << a[i][j] << " ";							
-		}
-		cout << endl;
-	}
-	cout << endl << " b after" << endl;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			cout << b[i][j] << " ";							
-		}
-		cout << endl;
-	}
-	
-	
 
-	return 0;
+double** arr_init(int narr){
+	double **arr = new double*[narr];
+	for(int i=0;i<narr;i++){    
+    	arr[i] = new double[narr];    
+  	}
+  	return arr;
 }
 
+void in_arr(int narr, double **arr){	
+	for(int i=0;i<narr;i++){
+    	for(int j=0;j<narr;j++){	
+      		arr[i][j] = double(rand());
+    	}
+  	}
+}
+	
+void out_arr(int narr, double **arr){		
+	cout << endl;
+	for(int i=0;i<narr;i++){
+    	for(int j=0;j<narr;j++){
+      		cout << arr[i][j] << " ";
+    		}cout << endl;
+  	}
+  	cout << endl;
+}	
+
+void find_darr(int narr, double **arr2, float *arr1){	
+	for(int i=0;i<narr;i++){
+		for(int j=0;j<=narr;j++){				
+			if(i==j){
+				arr1[i] = arr2[i][j];
+			}
+		}		
+	}	
+}
+
+void sort_arr(int narr, float *arr1){
+	for(int i=1;i<narr;i++){
+		for(int j=0;j<=i;j++){
+			float xx;
+			if(arr1[i] < arr1[j]){
+				xx = arr1[i];
+				arr1[i] = arr1[j];
+				arr1[j] = xx;
+			}
+		}
+	}
+}
+
+void out_arr1(int narr, float *arr1){
+	cout << endl;
+	for(int i=0;i<narr;i++){
+		cout << arr1[i] << " ";	
+	}
+	cout << endl;
+}
+
+void zam_darr(int narr, double **arr2, float * arr1){
+	for(int i=0;i<narr;i++){
+		arr2[i][i] = arr1[i];		
+	}
+}
+
+	
+int main (){
+	int n;	
+	cin >> n;
+	srand(time(NULL));
+	
+	double **arr_a = arr_init(n);
+	in_arr(n, arr_a);
+	out_arr(n, arr_a);
+	
+	double **arr_b = arr_init(n);
+	in_arr(n, arr_b);
+	out_arr(n, arr_b);
+	
+	float *arr_a1 = new float[n];
+	float *arr_b1 = new float[n];
+	
+	find_darr(n,arr_a,arr_a1);
+	sort_arr(n,arr_a1);			out_arr1(n,arr_a1);
+	
+	find_darr(n,arr_b,arr_b1);
+	sort_arr(n,arr_b1);			out_arr1(n,arr_b1);
+	
+	zam_darr(n, arr_a, arr_b1);
+	zam_darr(n, arr_b, arr_a1);
+	
+	
+	out_arr(n, arr_a);
+	out_arr(n, arr_b);	
+	
+	return 0;
+}
